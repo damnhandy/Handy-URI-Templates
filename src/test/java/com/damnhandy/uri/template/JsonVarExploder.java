12f7a59a47cef10e1c0f29e1dbd940f37f73867e
Ryan J. McDonough <ryan@damnhandy.com>
2012-04-22 19:14:31 -0400

    Added tests with a custom VarExploder using Jackson

---
+++ src/test/java/com/damnhandy/uri/template/JsonVarExploder.java
@@ -0,0 +1,90 @@
+/*
+ * 
+ */
+package com.damnhandy.uri.template;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+import com.damnhandy.uri.template.VarExploder;
+import com.damnhandy.uri.template.impl.VariableExpansionException;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * An example exploder that reads JSON data and explodes into a URI template.
+ * 
+ * @author <a href="ryan@damnhandy.com">Ryan J. McDonough</a>
+ * @version $Revision: 1.1 $
+ */
+public class JsonVarExploder implements VarExploder
+{
+   
+   private InputStream source;
+   
+   private Map<String, Object> values;
+   
+   /**
+    * 
+    * Create a new JsonVarExploder.
+    * 
+    * @param file
+    * @throws FileNotFoundException
+    */
+   public JsonVarExploder(File file) throws FileNotFoundException
+   {
+      this(new FileInputStream(file));
+   }
+   
+   public JsonVarExploder(String jsonText) 
+   {
+      this(new ByteArrayInputStream(jsonText.getBytes()));
+   }
+   
+   /**
+    * 
+    * Create a new JsonVarExploder.
+    * 
+    * @param in
+    */
+   public JsonVarExploder(InputStream in)
+   {
+      this.source = in;
+      initValues();
+   }
+   
+   private void initValues() 
+   {
+      ObjectMapper mapper = new ObjectMapper();
+      try
+      {
+         values = mapper.readValue(source, new TypeReference<Map<String, Object>>() { });
+      }
+      catch (JsonParseException e)
+      {
+         throw new VariableExpansionException(e);
+      }
+      catch (JsonMappingException e)
+      {
+         throw new VariableExpansionException(e);
+      }
+      catch (IOException e)
+      {
+         throw new VariableExpansionException(e);
+      }
+   }
+   
+   @Override
+   public Map<String, Object> getNameValuePairs()
+   {
+      return values;
+   }
+
+}
---
+++ src/test/java/com/damnhandy/uri/template/TestCustomVarExploder.java
@@ -0,0 +1,41 @@
+/*
+ * 
+ */
+package com.damnhandy.uri.template;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import com.damnhandy.uri.template.UriTemplate;
+
+/**
+ * A TestCustomVarExploder.
+ * 
+ * @author <a href="ryan@damnhandy.com">Ryan J. McDonough</a>
+ * @version $Revision: 1.1 $
+ */
+public class TestCustomVarExploder
+{
+
+   private static final String EXPLODE_TEMPLATE = "/mapper{?address*}";
+   
+   private static final String JSON = "{\"city\": \"Boston\", " +
+   		                               "\"country\": \"USA\", " +
+   		                               "\"state\": \"MA\", " +
+   		                               "\"street\": \"4 Yawkey Way\", " +
+   		                               "\"zipcode\": \"02215-3496\" }";
+
+   @Test
+   public void testWrappedExploder()
+   {
+      Map<String, Object> values = new HashMap<String, Object>();
+      values.put("address", new JsonVarExploder(JSON));
+      String result = UriTemplate.expand(EXPLODE_TEMPLATE, values);
+      Assert.assertEquals("/mapper?city=Boston&country=USA&state=MA&street=4%20Yawkey%20Way&zipcode=02215-3496", result);
+      
+   }
+}
