ba8c63746cd0debccb4d94901804d40b6c795d84
Ryan J. McDonough <ryan@damnhandy.com>
2012-04-27 07:30:20 -0400

    Removed ExplodeWith annotation, not sure it's really necessary Updated pom.xml for Sonatype deployment Tweaked docs

--- src/main/java/com/damnhandy/uri/template/impl/VarExploderFactory.java
+++ src/main/java/com/damnhandy/uri/template/impl/VarExploderFactory.java
@@ -3,21 +3,17 @@
  */
 package com.damnhandy.uri.template.impl;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-
-import com.damnhandy.uri.template.ExplodeWith;
 import com.damnhandy.uri.template.VarExploder;
 
 /**
- * A VarExploderFactory.
+ * 
  * 
  * @author <a href="ryan@damnhandy.com">Ryan J. McDonough</a>
  * @version $Revision: 1.1 $
  */
 public final class VarExploderFactory
 {
-   private static final String DEFAULT_MSG = "";
+   //private static final String DEFAULT_MSG = "";
 
    private VarExploderFactory()
    {
@@ -36,93 +32,70 @@ public final class VarExploderFactory
          return (VarExploder) varValue;
       }
 
-      if (varValue.getClass().isAnnotationPresent(ExplodeWith.class))
-      {
-         Class<?> exploderClass = varValue.getClass().getAnnotation(ExplodeWith.class).value();
-         return fromCustom(exploderClass, varValue, varSpec);
-      }
+//      if (varValue.getClass().isAnnotationPresent(ExplodeWith.class))
+//      {
+//         Class<?> exploderClass = varValue.getClass().getAnnotation(ExplodeWith.class).value();
+//         return fromCustom(exploderClass, varValue, varSpec);
+//      }
       return new DefaultVarExploder(varValue);
    }
 
-   /**
-    * FIXME Comment this
-    * 
-    * @param exploderClass
-    * @param varValue
-    * @return
-    */
-   private static VarExploder fromCustom(Class<?> exploderClass, Object varValue, VarSpec varSpec)
-   {
-      if (exploderClass.isAssignableFrom(VarExploder.class))
-      {
-         String msg = buildErrorMessage(
-               exploderClass.getSimpleName() + " must be an instance of " + VarExploder.class.getSimpleName(),
-               exploderClass, varValue, varSpec);
-         throw new VariableExpansionException(msg);
-      }
 
-      try
-      {
-         Constructor<?> constructor = exploderClass.getConstructor(Object.class);
-         VarExploder exploder = (VarExploder) constructor.newInstance(varValue);
-         return exploder;
-      }
+//   private static VarExploder fromCustom(Class<?> exploderClass, Object varValue, VarSpec varSpec)
+//   {
+//      if (exploderClass.isAssignableFrom(VarExploder.class))
+//      {
+//         String msg = buildErrorMessage(
+//               exploderClass.getSimpleName() + " must be an instance of " + VarExploder.class.getSimpleName(),
+//               exploderClass, varValue, varSpec);
+//         throw new VariableExpansionException(msg);
+//      }
+//
+//      try
+//      {
+//         Constructor<?> constructor = exploderClass.getConstructor(Object.class);
+//         VarExploder exploder = (VarExploder) constructor.newInstance(varValue);
+//         return exploder;
+//      }
+//
+//      catch (IllegalArgumentException e)
+//      {
+//         throwException(DEFAULT_MSG, e, exploderClass, varValue, varSpec);
+//      }
+//      catch (NoSuchMethodException e)
+//      {
+//         throwException(DEFAULT_MSG, e, exploderClass, varValue, varSpec);
+//      }
+//      catch (InstantiationException e)
+//      {
+//         throwException(DEFAULT_MSG, e, exploderClass, varValue, varSpec);
+//      }
+//      catch (IllegalAccessException e)
+//      {
+//         throwException(DEFAULT_MSG, e, exploderClass, varValue, varSpec);
+//      }
+//      catch (InvocationTargetException e)
+//      {
+//         throwException(DEFAULT_MSG, e, exploderClass, varValue, varSpec);
+//      }
+//      return null;
+//   }
 
-      catch (IllegalArgumentException e)
-      {
-         throwException(DEFAULT_MSG, e, exploderClass, varValue, varSpec);
-      }
-      catch (NoSuchMethodException e)
-      {
-         throwException(DEFAULT_MSG, e, exploderClass, varValue, varSpec);
-      }
-      catch (InstantiationException e)
-      {
-         throwException(DEFAULT_MSG, e, exploderClass, varValue, varSpec);
-      }
-      catch (IllegalAccessException e)
-      {
-         throwException(DEFAULT_MSG, e, exploderClass, varValue, varSpec);
-      }
-      catch (InvocationTargetException e)
-      {
-         throwException(DEFAULT_MSG, e, exploderClass, varValue, varSpec);
-      }
-      return null;
-   }
 
-   /**
-    * 
-    * 
-    * @param message
-    * @param e
-    * @param exploderClass
-    * @param varValue
-    * @param varSpec
-    * @throws VariableExpansionException
-    */
-   private static void throwException(String message, Throwable e, Class<?> exploderClass, Object varValue,
-         VarSpec varSpec) throws VariableExpansionException
-   {
-      String msg = buildErrorMessage(message, exploderClass, varValue, varSpec);
-      throw new VariableExpansionException(msg, e);
-   }
+//   private static void throwException(String message, Throwable e, Class<?> exploderClass, Object varValue,
+//         VarSpec varSpec) throws VariableExpansionException
+//   {
+//      String msg = buildErrorMessage(message, exploderClass, varValue, varSpec);
+//      throw new VariableExpansionException(msg, e);
+//   }
 
-   /**
-    * FIXME Comment this
-    * 
-    * @param exploderClass
-    * @param varValue
-    * @param varSpec
-    * @return
-    */
-   private static String buildErrorMessage(String message, Class<?> exploderClass, Object varValue, VarSpec varSpec)
-   {
-      StringBuilder b = new StringBuilder();
-      b.append(message)
-       .append("[ varspec: ").append(varSpec)
-       .append(" variable value:").append(varValue)
-       .append(" exploder class: ").append(exploderClass.getName()).append("]");
-      return b.toString();
-   }
+//   private static String buildErrorMessage(String message, Class<?> exploderClass, Object varValue, VarSpec varSpec)
+//   {
+//      StringBuilder b = new StringBuilder();
+//      b.append(message)
+//       .append("[ varspec: ").append(varSpec)
+//       .append(" variable value:").append(varValue)
+//       .append(" exploder class: ").append(exploderClass.getName()).append("]");
+//      return b.toString();
+//   }
 }
--- src/main/java/com/damnhandy/uri/template/package-info.java
+++ src/main/java/com/damnhandy/uri/template/package-info.java
@@ -2,7 +2,10 @@
  * <h1>Handy URI Templates</h1>
  * 
  * <p>
- * This package contains the core classes for working with URI Templates.
+ * This package contains the core classes for manipulating and processing different types
+ * of URI Templates. At present, it is only supports <a href="http://tools.ietf.org/html/rfc6570">RFC6570 URI Templates</a>. 
+ * Other URI Templates, such as <a href="http://www.opensearch.org/Home">OpenSearch</a> are being 
+ * considered as a future enhancement.
  * </p>
  * @since 1.0.0
  * @author <a href="mailto:ryan@damnhandy.com">Ryan J. McDonough</a>
