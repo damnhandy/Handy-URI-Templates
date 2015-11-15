10e20d75e2746829e3f6b105f463ada13a64b887
Ryan J. McDonough <ryan@damnhandy.com>
2015-11-14 22:03:00 -0500

    changes for issue #42

---
+++ src/test/java/com/damnhandy/uri/template/AbstractUriTemplateBuilderTests.java
@@ -0,0 +1,47 @@
+package com.damnhandy.uri.template;
+
+import com.damnhandy.uri.template.conformance.AbstractUriTemplateConformanceTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Map;
+
+/**
+ * @author <a href="ryan@damnhandy.com">Ryan J. McDonough</a>
+ * Created by ryan on 11/14/15.
+ */
+public class AbstractUriTemplateBuilderTests extends AbstractUriTemplateConformanceTest
+{
+
+    /**
+     * @param vars
+     * @param template
+     * @param expected
+     * @param testsuite
+     */
+    public AbstractUriTemplateBuilderTests(Map<String, Object> vars, String template, Object expected, String testsuite)
+    {
+        super(vars, template, expected, testsuite);
+    }
+
+    /**
+     *
+     * @throws Exception
+     */
+    @Test
+    public void test() throws Exception
+    {
+        UriTemplate original = UriTemplate.fromTemplate(template);
+        UriTemplateBuilder originalBuilder = UriTemplate.buildFromTemplate(original);
+        UriTemplateComponent[] components = originalBuilder.getComponents();
+
+        UriTemplateBuilder builder = UriTemplate.createBuilder();
+        for(UriTemplateComponent c : components)
+        {
+            builder.addComponent(c);
+        }
+        UriTemplate rebuiltTemplate = builder.build();
+
+        Assert.assertEquals(original.getTemplate(), rebuiltTemplate.getTemplate());
+    }
+}
--- src/test/java/com/damnhandy/uri/template/TestUriTemplateBuilderWithExtendedTests.java
+++ src/test/java/com/damnhandy/uri/template/TestUriTemplateBuilderWithExtendedTests.java
@@ -1,27 +1,22 @@
 package com.damnhandy.uri.template;
 
-import com.damnhandy.uri.template.*;
-import com.damnhandy.uri.template.conformance.AbstractUriTemplateConformanceTest;
-import org.junit.Assert;
-import org.junit.Test;
 import org.junit.runners.Parameterized;
 
 import java.io.File;
 import java.util.Collection;
-import java.util.LinkedList;
 import java.util.Map;
 
 /**
  * @author <a href="ryan@damnhandy.com">Ryan J. McDonough</a>
  * Created by ryan on 11/14/15.
  */
-public class TestUriTemplateBuilderWithConformanceTests extends AbstractUriTemplateBuilderTests
+public class TestUriTemplateBuilderWithExtendedTests extends AbstractUriTemplateBuilderTests
 {
 
     @Parameterized.Parameters(name = "{1}")
     public static Collection<Object[]> testData() throws Exception
     {
-        File file = new File("./uritemplate-test/spec-examples.json");
+        File file = new File("./uritemplate-test/extended-tests.json");
         return loadTestData(file);
     }
 
@@ -31,10 +26,8 @@ public class TestUriTemplateBuilderWithConformanceTests extends AbstractUriTempl
      * @param expected
      * @param testsuite
      */
-    public TestUriTemplateBuilderWithConformanceTests(Map<String, Object> vars, String template, Object expected, String testsuite)
+    public TestUriTemplateBuilderWithExtendedTests(Map<String, Object> vars, String template, Object expected, String testsuite)
     {
         super(vars, template, expected, testsuite);
     }
-
-
 }
