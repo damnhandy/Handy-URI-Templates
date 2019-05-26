5f3f731fe12b62dcd40b02818573ba9e8e9c1d37
Ryan J. McDonough <ryan@damnhandy.com>
2019-05-25 18:32:29 -0700

    Wagon: Deploying site to repository

--- apidocs/package-search-index.js
+++ apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"com.damnhandy.uri.template"},{"l":"com.damnhandy.uri.template.impl"},{"l":"com.damnhandy.uri.template.jackson.datatype"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.damnhandy.uri.template"},{"l":"com.damnhandy.uri.template.impl"},{"l":"com.damnhandy.uri.template.jackson.datatype"}]
